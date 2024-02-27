2912dfb551e5c467c9f8b78678a94d60c5869871
Sifiso Cyprian Shezi <sifiso.cyprianshezi28@gmail.com>
2024-02-27 20:47:04 +0200

    Add code for practicing strings

--- MyClass.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-public class MyClass
-{
-    public void PrintMessage()
-    {
-        Console.WriteLine("Hello SDET Things Team!");
-    }
-}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, SDET Things Team!");
+
+string aFriend = "Simangaliso";
+Console.WriteLine(aFriend);
+
+aFriend = "Simangaliso";
+Console.WriteLine(aFriend);
+
+Console.WriteLine("Hello " + aFriend);
+
+string firstFriend = "Dumisani";
+string secondFriend = "Thebe";
+Console.WriteLine($"My friends are {firstFriend} and {secondFriend}");
+
+Console.WriteLine($"The name {firstFriend} has {firstFriend.Length} letters.");
+Console.WriteLine($"The name {secondFriend} has {secondFriend.Length} letters.");
+
+var myName = "Sifiso Cyprian Shezi";
+
+var firstLetterOfMyName = myName[0];
+var secondLetterOfMyName = myName[1];
+var eigthLetterOfMyName = myName[7];
+
+
+Console.WriteLine("The first letter of my name is: " + firstLetterOfMyName);
+
+Console.WriteLine("The second letter of my name is: " + secondLetterOfMyName);
+
+Console.WriteLine("The eighth letter in my name is: " + eigthLetterOfMyName);
+
+
+
+
